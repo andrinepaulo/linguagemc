885ee37cec75cda31b22a782f956f01bdcecb8be
andrinepaulo <andrinepaulo@outlook.com>
2024-09-10 14:32:47 +0000

    inicio do estudo em C

---
+++ hello.c
@@ -0,0 +1,13 @@
+// importando a biblioteca padrão de entrada e saída de dados
+// std(standard) i(input) o(output)
+#include <stdio.h>
+// Inicio do programa.
+int main(){
+// Comando para exibir o texto hello na tela.
+// O comando printf só executa porque você fez a importação da biblioteca stdio.
+    printf("Hello World\n");
+
+// O comando abaixo imforma ao compilador que o programa acabou
+    return 0;
+
+}
