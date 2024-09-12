bd87ae0fedef11b7213d0371961055bca84f0399
andrinepaulo <andrinepaulo@outlook.com>
2024-09-12 13:20:33 +0000

    sofrendo na aula

---
+++ rodizio_switch.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int final_placa;
+    printf("Digite o número final da placa do seu veículo:\n");
+    scanf("%d",%final_placa);
+
+    switch(final_placa){
+        case 1:
+        printf("Rodízio na segunda-feira\n");
+        break;
+        
+    }
+}
